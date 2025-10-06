30db0980ad251129e80e0ddabeddf1ecbfbd471b
DiegoAmaya19 <141657295+DiegoAmaya19@users.noreply.github.com>
2025-10-05 20:22:02 -0500

    Avances en el servicio de Angular

---
+++ AgendaTelefonica/AgendaTelelefonica_FrontEnd/AgendaTelefonicaAngular/src/app/Model/contacto-request.ts
@@ -0,0 +1,12 @@
+export interface ContactoRequest {
+    id: number;
+    primerNombre: string;
+    segundoNombre: string;
+    primerApellido: string;
+    segundoApellido: string;
+    grupo: string;
+    direccion: string;
+    email: string;
+    indicativo: string;
+    numero: string;
+}
---
+++ AgendaTelefonica/AgendaTelelefonica_FrontEnd/AgendaTelefonicaAngular/src/app/Services/datos.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root' //Lo hace disponible globalmente singleton
+})
+export class DatosService {
+
+  constructor() { }
+}
