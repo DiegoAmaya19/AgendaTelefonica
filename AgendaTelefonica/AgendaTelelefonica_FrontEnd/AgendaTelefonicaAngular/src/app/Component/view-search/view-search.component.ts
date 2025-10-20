debf196c9725524b0123e765ac054923b2ebf860
DiegoAmaya19 <141657295+DiegoAmaya19@users.noreply.github.com>
2025-10-19 21:45:43 -0500

    Nuevo Buscar

---
+++ AgendaTelefonica/AgendaTelelefonica_FrontEnd/AgendaTelefonicaAngular/src/app/Component/view-search/view-search.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-view-search',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './view-search.component.html',
+  styleUrl: './view-search.component.css'
+})
+export class ViewSearchComponent {
+
+}
--- AgendaTelefonica/AgendaTelelefonica_FrontEnd/AgendaTelefonicaAngular/src/app/app.routes.ts
+++ AgendaTelefonica/AgendaTelelefonica_FrontEnd/AgendaTelefonicaAngular/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ViewGetComponent } from './Component/view-get/view-get.component';
 import { ViewPostComponent } from './Component/view-post/view-post.component';
 import { MainComponent } from './Component/view-main/main/main.component';
+import { ViewSearchComponent } from './Component/view-search/view-search.component';
 
 export const routes: Routes = [
     {path: '', component: MainComponent},
     {path: 'main', component: MainComponent},
     {path: 'viewGetRuta', component: ViewGetComponent},
-    {path: 'viewPostRuta', component: ViewPostComponent}
+    {path: 'viewPostRuta', component: ViewPostComponent},
+    {path: 'viewSearchRuta', component: ViewSearchComponent}
 ];
